601ed7795b43b51fa9d47234d616294290b1ed06
Votre Nom <stephanielabe14@gmail.com>
2024-12-11 15:23:02 +0100

    First commit

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -1,11 +1,22 @@
-const express = require('express');
+console.log("server started")
+if(process.env.NODE_ENV !== 'production'){
+	require('dotenv').config({path: "./.env"})
+}
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const config = require("./config/config")
+const { sequelize } = require("./models")
 const app = express();
+app.use(morgan("combined"));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use('/public', express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+require('./passport')
+require("./routers/routes")(app)
+sequelize.sync({ force: false })
+	.then(() => {
+		app.listen(config.port, () => console.log(`Express server running on port ${config.port}`));
+	})
